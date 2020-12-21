331743dab090983ec6ed4ff73ba970dd2d719b28
Scott Doxey <neogeek0101@gmail.com>
2020-12-21 01:21:38 -0500

    Initial commit.

---
+++ ObjectDragInteraction.cs
@@ -0,0 +1,115 @@
+using System;
+using CandyCoded;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ScottDoxey.Interactions
+{
+
+    [Serializable]
+    public class ObjectInteractionReleaseEvent : UnityEvent<Vector3, Vector3, Vector3, Vector3>
+    {
+
+    }
+
+    public class ObjectDragInteraction : MonoBehaviour
+    {
+
+        private const float DAMPEN_INPUT_POSITION_SPEED = 0.01f;
+
+        public ObjectInteractionReleaseEvent Released;
+
+        private int? _currentFingerId;
+
+        private Vector3? _dampenedInputPosition;
+
+        private float? _dragStartDistance;
+
+        private Vector3? _dragStartOffset;
+
+        private Vector3? _dragStartPosition;
+
+        private Vector3? _lastInputPosition;
+
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+
+            _mainCamera = Camera.main;
+
+        }
+
+        private void Update()
+        {
+
+            if (gameObject.GetInputDown(_mainCamera, ref _currentFingerId, out RaycastHit hit))
+            {
+
+                _dampenedInputPosition = InputManager.GetInputPosition(_currentFingerId);
+
+                _dragStartPosition = gameObject.transform.position;
+
+                _dragStartDistance = hit.distance;
+
+                _dragStartOffset = _dragStartPosition.Value - hit.point;
+
+            }
+
+            if (_dragStartPosition.HasValue)
+            {
+
+                _lastInputPosition = InputManager.GetInputPosition(_currentFingerId);
+
+                if (_lastInputPosition.HasValue && _dragStartDistance.HasValue && _dragStartOffset.HasValue)
+                {
+
+                    gameObject.transform.position =
+                        _mainCamera.ScreenPointToRay(_lastInputPosition.Value).GetPoint(_dragStartDistance.Value) +
+                        _dragStartOffset.Value;
+
+                }
+
+                if (_lastInputPosition.HasValue && _dampenedInputPosition.HasValue)
+                {
+
+                    _dampenedInputPosition = Vector3.Lerp(_dampenedInputPosition.Value, _lastInputPosition.Value,
+                        DAMPEN_INPUT_POSITION_SPEED);
+
+                }
+
+            }
+
+            if (InputManager.GetInputUp(ref _currentFingerId))
+            {
+
+                if (_lastInputPosition.HasValue && _dampenedInputPosition.HasValue && _dragStartPosition.HasValue &&
+                    _dragStartDistance.HasValue)
+                {
+
+                    var lastInputPositionWorld = _mainCamera.ScreenToWorldPoint(new Vector3(_lastInputPosition.Value.x,
+                        _lastInputPosition.Value.y, _dragStartDistance.Value));
+
+                    var dampenedInputPositionWorld = _mainCamera.ScreenToWorldPoint(
+                        new Vector3(_dampenedInputPosition.Value.x, _dampenedInputPosition.Value.y,
+                            _dragStartDistance.Value));
+
+                    var velocity = lastInputPositionWorld - dampenedInputPositionWorld;
+
+                    Released?.Invoke(_dragStartPosition.Value, gameObject.transform.position, velocity, Vector3.zero);
+
+                }
+
+                _dragStartPosition = null;
+
+                _dragStartDistance = null;
+
+                _dragStartOffset = null;
+
+            }
+
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# ObjectDragInteraction
+
+> A simple component that adds dragging interaction to any object without config.
+
+### Unity Package Manager
+
+<https://docs.unity3d.com/Packages/com.unity.package-manager-ui@2.0/manual/index.html>
+
+#### Git
+
+```json
+{
+  "dependencies": {
+    "com.scottdoxey.objectdraginteraction": "https://github.com/neogeek/ObjectDragInteraction.git#v1.0.0",
+    ...
+  }
+}
+```
